d904c3eea344278ff489980d9f70f2885854ead0
tony <effrenanthony2704@gmail.com>
2020-10-10 11:45:01 -0500

    semana12 dias6

--- semana12/dia6/demoproyectos/api/models.py
+++ semana12/dia6/demoproyectos/api/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
+
 # Create your models here.
+class Cliente(AbstractUser):
+    # si dejamos en blank true los campos no son obligatorios
+    altura = models.FloatField(blank=True, null=True)
+    peso = models.FloatField(blank=True, null=True)
+    fnaciamiento = models.DateTimeField(blank=True, null=True)
+    actividadfisica = models.IntegerField(blank=True, null=True)
--- semana12/dia6/demoproyectos/demoproyectos/settings.py
+++ semana12/dia6/demoproyectos/demoproyectos/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'api.Cliente'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
